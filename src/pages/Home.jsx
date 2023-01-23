b06567af6d1e12985ca48bef33e11babff8d7b26
ConradGuillaume <conrad.guillaume0501@gmail.com>
2023-01-23 08:50:33 +0100

    r

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,13 @@ import Crew from "../components/Crew";
 import Footer from "../components/Footer";
 import LandingPage from "../components/LandingPage";
 
-import NavBar from "../components/NavBar";
+//import NavBar from "../components/NavBar";
 import Services from "../components/Services";
 import Testimonial from "../components/Testimonial";
 
 const Home = () => {
   return (
     <div className="homeContainer">
-      <NavBar />
       <LandingPage />
       <Business />
       <Services />
