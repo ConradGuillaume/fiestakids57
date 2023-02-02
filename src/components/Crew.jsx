924c3b635ed1410387334368caa6c9c53566fdb8
ConradGuillaume <conrad.guillaume0501@gmail.com>
2023-02-02 15:52:43 +0100

    new_feature

--- src/components/Crew.jsx
+++ src/components/Crew.jsx
@@ -1,35 +1,38 @@
 import React from "react";
-import tache2 from "../style/img/backpic/tache2.svg";
+
 const Crew = () => {
   return (
     <div className="crewContainer">
       <h3>Des animations pour tout les ages !</h3>
       <article className="ages">
-        <img src={tache2} alt="" />
-        <p>
+        <p className="text">
           Nous nous occupons aussi bien des tout petits que des plus grands pour
           vous offrir un maximum de sérénité ! ​ Nous prenons en charge les
           enfants de 0 à 12 ans !
         </p>
       </article>
       <article className="crew">
-        <div className="theCrew">
-          <img src={tache2} alt="" />
-          <p className="crewTitle">Une équipe diplomée ! </p>
-          <p>
+        <div class="card">
+          <div class="circle"></div>
+          <div class="circle"></div>
+          <p className="title">Une équipe diplomée !</p>
+          <p className="undertitle">
             Les animatrices ont plusieurs années d’expérience dans le métier de
             l'animation et sont toutes diplômées ! BAFA, BAFD, CAP petite
             enfance, Master métier de l'enseignement et CRPE . Quoi de mieux ?
           </p>
+          <div class="card-inner"></div>
         </div>
-        <div className="cost">
-          <img src={tache2} alt="" />
-          <p className="crewTitle">Des tarifs compétitifs</p>
-          <p>
+        <div class="card">
+          <div class="circle"></div>
+          <div class="circle"></div>
+          <p className="title">Des tarifs compétitifs</p>
+          <p className="text">
             Nos tarifs sont accessibles et calculés en fonction de votre nombre
             d'enfants. Le plus c'est que vous n'avez rien à penser ! Nous nous
             occupons de tout et fournissons tout le matériel nécessaire !
           </p>
+          <div class="card-inner"></div>
         </div>
       </article>
       <aside className="contactBtn ">
